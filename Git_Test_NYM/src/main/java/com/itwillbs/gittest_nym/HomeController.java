93407c17508e624974c0d7550f4b010f1e04843b
NYM <dash8956@gmail.com>
2024-11-05 17:27:17 +0900

    gggg

--- Git_Test_NYM/src/main/java/com/itwillbs/gittest_nym/HomeController.java
+++ Git_Test_NYM/src/main/java/com/itwillbs/gittest_nym/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+dddd
 /**
  * Handles requests for the application home page.
  */
