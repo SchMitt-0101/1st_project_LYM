7281fc1f1945699538bbd8f450b92f5a148ebc25
KJH <janedev27@gmail.com>
2024-11-05 16:59:49 +0900

    test

--- Git_Test_NYM/src/main/java/com/itwillbs/gittest_nym/HomeController.java
+++ Git_Test_NYM/src/main/java/com/itwillbs/gittest_nym/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+	// 주석주석!!!
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
